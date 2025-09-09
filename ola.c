6703fbec2bc12bcfe3ece6bd337c8e4388952685
washingtonsousa12 <washingtonsousa12@gmail.com>
2025-09-09 20:39:43 -0300

    Update ola.c
    Incluindo o código inicial

--- ola.c
+++ ola.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+int main(){
+    float salario, novoSalario;
+    printf("Digite o salário: ");
+    scanf("%f" ,&salario);
+    if(salario <= 2000){
+        novoSalario = salario * 1.30;
+    }
+    else{
+        novoSalario = salario * 1.10;
+    }
+    printf("Novo salário = R$%.2f",novoSalario);
+
+    return 0;
+}
